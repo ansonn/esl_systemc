68ba5d895da98853296b9442dbf596430690783a
ansonn.wang <ansonn.wang@foxmail.com>
2014-05-28 17:21:24 +0800

    doc update

--- project/TestSC/src/TestSC.cpp
+++ project/TestSC/src/TestSC.cpp
@@ -12,7 +12,19 @@ using namespace std;
 
 int sc_main(int, char *[])
 {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	sc_uint<13> a = 0;
+	sc_int<32> b = -1;
+	sc_int<32> c = 90;
+	sc_bv<6> d = "110010";
+	sc_lv<6> e = "1100X0";
+
+	a = b;
+
+	cout << "a=" << a << endl;
+	cout << "c=" << c << endl;
+	cout << "d=" << d << endl;
+	cout << "e=" << e << endl;
+
 	return 0;
 
 }
